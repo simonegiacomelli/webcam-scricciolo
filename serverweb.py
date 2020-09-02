7ed9cb6e68850ac66e3b1e5385a47802943a858f
Simone Giacomelli <simone.giacomelli@gmail.com>
2020-09-02 13:06:01 +0200

    basic authentication

--- serverweb.py
+++ serverweb.py
@@ -1,8 +1,10 @@
+import base64
 import os
 import json
 import sys
 from functools import partial
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from pathlib import Path
 from typing import List, Dict, Callable
 from urllib.parse import urlparse, parse_qs
 
@@ -11,6 +13,7 @@ from webcam import Metadata, File
 
 class RequestHandler(SimpleHTTPRequestHandler):
     metadata: Metadata = None
+    auth_file = Path('.auth.txt')  # username:password see RFC 7617
     query_index = 4
     path_index = 2
     api: Dict[str, Callable] = None
@@ -52,6 +55,9 @@ class RequestHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         params, rpath = self.decode_request()
         if rpath.startswith('/api/'):
+            if not self.authorized():
+                self.send_string('not authorized', code=401)
+                return
             api_method = self.api.get('API_' + rpath[5:], None)
             if api_method is not None:
                 api_method(self, **params)
@@ -71,12 +77,22 @@ class RequestHandler(SimpleHTTPRequestHandler):
     def send_json(self, obj):
         self.send_string(json.dumps(obj, indent=2))
 
-    def send_string(self, message):
+    def send_string(self, message, code=200):
         self.protocol_version = "HTTP/1.1"
-        self.send_response(200)
+        self.send_response(code)
         self.send_header("Content-Length", str(len(message)))
+        self.send_header("WWW-Authenticate", "Basic")
         self.end_headers()
         self.wfile.write(bytes(message, "utf8"))
+
+    def authorized(self):
+        if not self.auth_file.exists():
+            return True
+        auth_raw = self.headers.get('Authorization', '').split(' ')
+        if len(auth_raw) != 2 or auth_raw[0] != 'Basic':
+            return False
+        auth_decoded = base64.b64decode(auth_raw[1]).decode('utf-8')
+        return auth_decoded == self.auth_file.read_text()
 
 
 def main():
